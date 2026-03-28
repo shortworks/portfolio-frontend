583acafcc3c23acd7944a1c1016aca67731cb782
shortnr <shortnr@hotmail.com>
2026-03-27 22:11:53 -0700

    added some content to the home page; added placeholder for projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
+import AboutMe from './pages/AboutMe'
+import Projects from './pages/Projects'
 import Resume from './pages/Resume'
 import ContactMe from './pages/ContactMe'
-import AboutMe from './pages/AboutMe'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="mx-auto flex min-h-screen max-w-3xl flex-grow flex-col md:max-w-4xl">
-        <Navbar />
+      <Navbar />
+      <div className="mx-auto flex min-h-screen max-w-3xl flex-grow flex-col overflow-x-hidden md:max-w-4xl">
         <div className="flex">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<ContactMe />} />
           </Routes>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,18 @@
+import Logo from '../components/Logo'
 
 export default function HomePage() {
-    return (
-        <div>
-            This is the home page.
+  return (
+    <div className="mx-auto flex flex-grow flex-col items-center justify-between gap-6">
+      <div className="hero-wrap bg-card-blur mt-4 w-full">
+        <div className="hero-content">
+          <h2 className="font-header text-primary mb-2 font-bold">
+            <Logo size="text-2xl sm:text-4xl md:text-6xl" />
+          </h2>
+          <p className="text-secondary text-md md:text-2xl">
+            hardware + software engineer, portland or
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
